8e81dc9c4f5345e5896260187840df659b0e0896
einar <unico.arkngl.97@gmail.com>
2022-05-31 11:45:30 -0400

    deploy corregido

---
+++ build/precache-manifest.83587d76b3b23fb1abe1ee6c7a9ac414.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ad9eafa44d9bab0a81a",
+    "url": "/static/css/main.9cc737d7.chunk.css"
+  },
+  {
+    "revision": "9ad9eafa44d9bab0a81a",
+    "url": "/static/js/main.9ad9eafa.chunk.js"
+  },
+  {
+    "revision": "2473ae3b2bb4ce736e65",
+    "url": "/static/js/1.2473ae3b.chunk.js"
+  },
+  {
+    "revision": "f02cf4206676f4bb9218",
+    "url": "/static/css/2.2fc4ce30.chunk.css"
+  },
+  {
+    "revision": "f02cf4206676f4bb9218",
+    "url": "/static/js/2.f02cf420.chunk.js"
+  },
+  {
+    "revision": "01704b94a7ed35268ef7",
+    "url": "/static/js/3.01704b94.chunk.js"
+  },
+  {
+    "revision": "0955121cbb3b9cc87c20",
+    "url": "/static/js/runtime~main.0955121c.js"
+  },
+  {
+    "revision": "2aeebe2e73112b070ce1fe89feb38a4c",
+    "url": "/static/media/logo-comunidad1.2aeebe2e.jpg"
+  },
+  {
+    "revision": "f80b77e79cb868aab2c8d1a48c373f9d",
+    "url": "/static/media/actividadDef.f80b77e7.png"
+  },
+  {
+    "revision": "a25e46a276d58cf0e15960704fb02f30",
+    "url": "/static/media/publicacionDef.a25e46a2.webp"
+  },
+  {
+    "revision": "ca9c5b622b20d8d93f9e6c720ee0ab43",
+    "url": "/static/media/voluntarioDef.ca9c5b62.webp"
+  },
+  {
+    "revision": "0cb5a02aa353f3d928b8cf17757670ed",
+    "url": "/static/media/imageAlter4.0cb5a02a.jpg"
+  },
+  {
+    "revision": "10f3591090ee78ec7d3a54481ae736b6",
+    "url": "/static/media/image1.10f35910.jpg"
+  },
+  {
+    "revision": "d339366d4b395b5d74d3ff2c842e2d7c",
+    "url": "/static/media/image2.d339366d.jpg"
+  },
+  {
+    "revision": "cd562cb2bdb13dae4beba6ec178d1d29",
+    "url": "/static/media/image3.cd562cb2.jpg"
+  },
+  {
+    "revision": "1a5f03e73cd9972a7190fa48b995a39e",
+    "url": "/static/media/imagenFondo.1a5f03e7.jpg"
+  },
+  {
+    "revision": "ab5d27d6d4867d41874719a5b93590dc",
+    "url": "/static/media/imagenFondoLogin.ab5d27d6.jpg"
+  },
+  {
+    "revision": "de378569e2e104b6bd24a6a736f2c943",
+    "url": "/static/media/imagenFondoRegister.de378569.jpg"
+  },
+  {
+    "revision": "fe4e895811420c806ece0fb060167ed2",
+    "url": "/static/media/abueloBailando.fe4e8958.gif"
+  },
+  {
+    "revision": "58b4da52d28a70033ece774861983325",
+    "url": "/index.html"
+  }
+];
